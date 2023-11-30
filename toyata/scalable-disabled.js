f9819c8f219fb7ac1167b410e3fdbd94fdf095ab
thomas <maries3321@gmail.com>
2023-11-30 18:32:48 +0800

    toyata v2.0

---
+++ toyata/scalable-disabled.js
@@ -0,0 +1,26 @@
+document.documentElement.addEventListener(
+  "touchstart",
+  function(event) {
+    if (event.touches.length > 1) {
+      event.preventDefault();
+    }
+  },
+  false
+);
+
+var lastTouchEnd = 0;
+document.documentElement.addEventListener(
+  "touchend",
+  function(event) {
+    var now = Date.now();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  },
+  false
+);
+
+document.addEventListener("gesturestart", function(event) {
+  event.preventDefault();
+});
